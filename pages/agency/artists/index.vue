e7c75b7c4a13ef2579805435a17afd6d4b52d10f
AchiUgulava <achiugulava@gmail.com>
2024-09-26 01:51:24 +0400

    nav gap

--- pages/agency/artists/index.vue
+++ pages/agency/artists/index.vue
@@ -27,41 +27,23 @@ console.log("nav:", navigation)
 <template>
   <div>
     <h1 class="p-5 absolute top-0 left-10 z-[10000]">
-      <nuxt-link
-        :to="localePath('/home')"
-        class="text-white font-bold text-3xl md:text-4xl "
-      >
+      <nuxt-link :to="localePath('/home')" class="text-white font-bold text-3xl md:text-4xl ">
         {{ $t('back') }}
       </nuxt-link>
     </h1>
     <tvOverlay image="url('/main.gif')" />
     <div class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
       <div
-        class="flex flex-col justify-center items-center gap-5 md:gap-4 bg-gray-600/40 rounded-xl border-gray-300/60 border p-3 md:w-[500px] lg:w-[800px] w-72"
-      >
+        class="flex flex-col justify-center items-center gap-5 md:gap-4 bg-gray-600/40 rounded-xl border-gray-300/60 border p-3 md:w-[500px] lg:w-[800px] w-72">
         <h1 class="text-white font-bold text-2xl md:text-6xl">
           ARTISTS
         </h1>
         <div
-          class="w-full flex-col justify-center items-center gap-5 md:gap-4 rounded-lg border-gray-200/70 border p-3 mb-1 flex h-96 lg:h-[500px] overflow-y-scroll"
-        >
-          <nav>
-            <ContentNavigation>
-              <ul>
-                <li
-                  v-for="link of navigation"
-                  :key="link._path"
-                >
-                  <NuxtLink
-                    :to="link._path"
-                    class="text-white font-bold text-lg md:text-5xl"
-                  >
-                    {{ link.title }}
-                  </NuxtLink>
-                </li>
-              </ul>
-            </ContentNavigation>
-          </nav>
+          class="w-full flex-col justify-center items-center gap-5  rounded-lg border-gray-200/70 border p-3 mb-1 flex h-96 lg:h-[500px] overflow-y-scroll">
+          <NuxtLink v-for="link of navigation" :key="link._path" :to="link._path"
+            class="text-white font-bold text-lg md:text-5xl">
+            {{ link.title }}
+          </NuxtLink>
         </div>
       </div>
     </div>
